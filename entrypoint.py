fba2d4ddf91d5c399c513ddcbd9aa640538d8b62
Dominik Krupke <krupke@ibr.cs.tu-bs.de>
2025-05-30 15:50:25 +0200

    fix: trying to fix issues with configuration via env vars.

--- entrypoint.py
+++ entrypoint.py
@@ -3,10 +3,26 @@ This file serves as the entrypoint for the docker container to start the FastAPI
 """
 
 import uvicorn
+import os
+import logging
 from server.sync import sync
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    handlers=[logging.StreamHandler()],
+)
 
 if __name__ == "__main__":
     # Sync the repository to the database
     sync()
     # Start the FastAPI application
-    uvicorn.run("server.app:app", reload=False, host="0.0.0.0", port=80)
+    IRB_ROOT = os.environ.get("IRB_ROOT", "")
+    logging.info("IRB_ROOT set to '%s'", IRB_ROOT)
+    uvicorn.run(
+        "server.app:app",
+        reload=False,
+        host="0.0.0.0",
+        port=80,
+        root_path=IRB_ROOT,
+    )
